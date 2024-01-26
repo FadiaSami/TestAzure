6b14eb4492b8cd405ba13f48209314683ae5dd79
FadiaSami <87935994+FadiaSami@users.noreply.github.com>
2024-01-26 21:27:43 +0300

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+
     <?php
 
 //    echo "Welcome". " ".  $_POST["FirstName"]. " ". $_POST["LasttName"];
@@ -30,5 +22,3 @@
     else
         echo mysqli_error($conn);
     ?>
-</body>
-</html>
